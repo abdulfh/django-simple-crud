b720217d9e268fddb4f4e9229079c3acc15be27b
fatah <abdul.hikami@byorange.co.id>
2023-08-17 17:07:39 +0700

    Fixing CORS

--- backend/settings.py
+++ backend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'cars',
     'orders',
     'authentication',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'django.contrib.auth',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -146,3 +148,8 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     "TOKEN_OBTAIN_SERIALIZER": "authentication.serializers.MyTokenObtainPairSerializer",
 }
+
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173',
+]
