29a28963f0f158466afd9d3744d3ff15a9b1479d
fatah <abdul.hikami@byorange.co.id>
2023-08-18 10:31:22 +0700

    Add Whitelisted URL for User

--- backend/middleware.py
+++ backend/middleware.py
@@ -13,6 +13,9 @@ class AuthenticatedMiddleware():
         "cars"
     ]
 
+    WHITELISTED_USER_ROUTE = [
+        "cars/list"
+    ]
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -21,16 +24,21 @@ class AuthenticatedMiddleware():
         return response
     
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.path in self.WHITELISTED_APP_NAME:
+        current_url = resolve(request.path_info).app_name
+        current_route = resolve(request.path_info).route
+
+        if current_url in self.WHITELISTED_APP_NAME:
             return None
         
         tokenString = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[1]
         accessToken = tokens.AccessToken(tokenString)
         request.user_id = accessToken.payload['user_id']
         request.is_admin = accessToken.payload['is_admin']
-        current_url = resolve(request.path_info).app_name
         
-        print(current_url)
+
+        if current_route in self.WHITELISTED_USER_ROUTE and request.is_admin != True:
+            return None
+        
         if current_url in self.WHITELISTED_ADMIN_APP_NAME and request.is_admin != True:
             return JsonResponse({"errors":"Not Found"}, safe=False,status=status.HTTP_404_NOT_FOUND)
         
