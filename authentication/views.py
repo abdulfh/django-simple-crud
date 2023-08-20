dfe50f0429dcbfa40980bbc25eebd579999b84bc
fatah <abdul.hikami@byorange.co.id>
2023-08-20 11:19:15 +0700

    Add Whitelisted register url

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
 from authentication.models import Admin
 from authentication.serializers import AdminSerializers, UserSerializers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 
 # Create your views here.
 @api_view(['POST'])
+@permission_classes([permissions.AllowAny])
 def user_register(request):
     serializer = UserSerializers(data=request.data)
     if serializer.is_valid():
@@ -17,6 +18,7 @@ def user_register(request):
 
 
 @api_view(['POST'])
+@permission_classes([permissions.AllowAny])
 def admin_register(request):
     serializer = AdminSerializers(data=request.data)
     if serializer.is_valid():
